a32d340dffa791a126325d55189dfe494456104d
zoharsanchez <lorena.zohar@gmail.com>
2016-09-29 12:13:00 -0700

    Add questions to master from chapter 1

---
+++ cracking-the-coding-interview-challenges/Chapter_1.js
@@ -0,0 +1,34 @@
+/*
+1.1
+isUnique: Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+ */
+const isUnique = str => {
+	// Write your code here!
+};
+
+/*
+1.2
+Check Permutation: Given two strings, write a method to decide if one is apermutation of the other.
+ */
+const checkPermutation = (str1, str2) => {
+	// Write your code here!
+};
+
+/*
+1.3
+URLify: Write a method to replace all spaces in a string with '%20'. You may assume that the string has sufficient space at the end to hold the additional characters, and that you are given the 'true' length of the string.
+I: "Mr John Smith "
+O: "Mr%20John%20Smith"
+ */
+const URLify = str => {
+	// Write your code here!
+};
+
+/*
+PalindromePermutation: Given a string, write a function to check if it is a permutation of a palindrome. A palindrome is a word or phrase that is the same forwards and backwards. A permutation is a rearrangement of letters. The palindrome does not need to be limited to just dictionary words.
+I: Tact Coa
+O: True (permutations: 'taco cat', 'atco cta', etc.)
+ */
+const palindromePer = str => {
+	// Write your code here!
+}; 
